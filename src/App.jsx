61c7cf7d0baa099ab438eb53aeb02d8d565bbf27
Parsa12458 <parsa.shirafkan127@gmail.com>
2024-11-12 20:18:44 +0330

    implement react router in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Homepage from "./pages/Homepage";
-import Footer from "./ui/Footer";
-import Header from "./ui/Header";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RecipesProvider } from "./context/RecipesContext";
 import { DarkModeProvider } from "./context/DarkModeContext";
 import DetailPage from "./pages/DetailPage";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
 
 const queryClient = new QueryClient();
 
@@ -15,12 +15,17 @@ function App() {
       <RecipesProvider>
         <DarkModeProvider>
           <ReactQueryDevtools initialIsOpen={false} />
-          <div className="overflow-x-hidden dark:bg-backgroundDark">
-            <Header />
-            {/* <Homepage /> */}
-            <DetailPage />
-            <Footer />
-          </div>
+          <BrowserRouter
+            future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+          >
+            <Routes>
+              <Route element={<AppLayout />}>
+                <Route path="/" element={<Navigate to="/home" replace />} />
+                <Route path="/home" element={<Homepage />} />
+                <Route path="/:recipeId" element={<DetailPage />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
         </DarkModeProvider>
       </RecipesProvider>
     </QueryClientProvider>
